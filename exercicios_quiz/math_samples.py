f555a41ebaeef560e386142da3ffc5d10f7acc40
Vicininho <65660066+Vicininho@users.noreply.github.com>
2023-05-15 21:39:35 +0000

    Adiciona a primeira versão das funções

---
+++ exercicios_quiz/math_samples.py
@@ -0,0 +1,13 @@
+class MathSamples:
+
+    @staticmethod
+    def double(n):
+        return None
+
+    @staticmethod
+    def power(base, potencia):
+        return None
+
+    @staticmethod
+    def factorial(n):
+        return None
