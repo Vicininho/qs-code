7b71fe3144acfe5244785b5941f8545006009555
Vicininho <65660066+Vicininho@users.noreply.github.com>
2023-05-15 21:51:33 +0000

    Abstração da função double, melhor cenário atingido

--- exercicios_quiz/math_samples.py
+++ exercicios_quiz/math_samples.py
@@ -2,12 +2,7 @@ class MathSamples:
 
     @staticmethod
     def double(n):
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return 4
+        return n * n
 
     @staticmethod
     def power(base, potencia):
