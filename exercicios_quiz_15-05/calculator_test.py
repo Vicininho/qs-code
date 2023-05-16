becee5fd88cc159ac10e23761342b4676750e9bb
Vicininho <65660066+Vicininho@users.noreply.github.com>
2023-05-16 00:29:54 +0000

    Renomeia primeiro caso de teste

--- exercicios_quiz_15-05/calculator_test.py
+++ exercicios_quiz_15-05/calculator_test.py
@@ -2,7 +2,7 @@ import unittest
 from calculator_samples import CalculatorSamples
 
 class CalculatorTest(unittest.TestCase):
-    def teste_1(self):
+    def teste_somar_1(self):
         """Testando caso para entradas 1 e 1"""
         self.assertEqual(
             CalculatorSamples.somar(1,1),
